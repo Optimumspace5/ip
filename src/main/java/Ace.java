c63bdb7585111ac36a35d5af4322fe77bde34399
Clarence Lee <clarencelee88@gmail.com>
2026-01-22 10:39:09 +0800

    Level-1: echo

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Ace {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,7 +8,18 @@ public class Ace {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello, I'm Ace.");
         System.out.println("What can i do for you?");
-        System.out.println("Bye, Hope to see you again soon");
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye, Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(userInput);
+        }
     }
 }
